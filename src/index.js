138ec9a22e683d3f153907edc0c2c4c7f7dc2e4e
syoung125 <laura120555@gmail.com>
2020-09-21 02:06:06 +0900

    Create Router.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 import firebase from "./firebase";
 
-console.log(firebase);
+console.log(firebase); // firebase 연결 확인
 
 ReactDOM.render(
   <React.StrictMode>
