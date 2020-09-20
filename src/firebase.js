bd9dfcb79bee5a151fc4484994c51e17fa71ed34
syoung125 <laura120555@gmail.com>
2020-09-21 00:30:26 +0900

    secure(not upload to gitHub) firebase keys by defining  process.env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
 import * as firebase from "firebase/app";
 
 // firebase 프로젝트 생성시 config 정보
+/**
+ * [#1.1 Securing the Keys]
+ * .env파일에 변수로 만들어줌 (REACT_APP_을 앞에 붙여야 하는게 rule)
+ * .env는 gitignore에 포함되어있으므로 내 정보를 github에 올리지 않음 (보안을 위함? ㄴㄴ 그냥 깃허브에만 올리지 않는 것)
+ * => 하지만 완전히 막을 수 있는 것은 아니다.
+ * 아예 숨길 수 있게 해주지 않음
+ */
 const firebaseConfig = {
-  apiKey: "AIzaSyBgLXjSaIl6kgMxnV4Te0hKl5Ah04cOlqs",
-  authDomain: "sytwitter-e3bdc.firebaseapp.com",
-  databaseURL: "https://sytwitter-e3bdc.firebaseio.com",
-  projectId: "sytwitter-e3bdc",
-  storageBucket: "sytwitter-e3bdc.appspot.com",
-  messagingSenderId: "981267419581",
-  appId: "1:981267419581:web:9a95967ff6800c233902d4",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
