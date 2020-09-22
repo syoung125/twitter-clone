a4546748f1fbed770bb1eae36b92ca475788aeab
syoung125 <laura120555@gmail.com>
2020-09-22 12:29:20 +0900

    Updates

---
+++ precache-manifest.f734830752ae0133c6b1a38a3a26a189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a24e08f8ca48970a21ec6b115bf7371",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "a27bdc2a7fbb7c69f13a",
+    "url": "/twitter-clone/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "e445f7e0d17ede923774",
+    "url": "/twitter-clone/static/js/2.2722c48a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/twitter-clone/static/js/2.2722c48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27bdc2a7fbb7c69f13a",
+    "url": "/twitter-clone/static/js/main.99a07ace.chunk.js"
+  },
+  {
+    "revision": "a857bf9adc4d9c37d9dc",
+    "url": "/twitter-clone/static/js/runtime-main.7327f089.js"
+  }
+]);
